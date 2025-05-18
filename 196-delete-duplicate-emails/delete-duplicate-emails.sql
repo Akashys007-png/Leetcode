73721ecd09db20fa7ad3f000e6bdaa241afe8438
Akashys007-png <akashys@csu.fullerton.edu>
2025-05-19 04:53:42 +0530

    Time: 886 ms (42.03%) | Memory: 0B (100.00%) - LeetSync

---
+++ 196-delete-duplicate-emails/delete-duplicate-emails.sql
@@ -0,0 +1,11 @@
+-- -- # Write your MySQL query statement below
+DELETE P FROM PERSON P
+JOIN PERSON P2
+ON P.EMAIL = P2.EMAIL WHERE P.ID > P2.ID;
+
+-- DELETE FROM Person
+-- WHERE id NOT IN (
+--     SELECT MIN(id)
+--     FROM Person
+--     GROUP BY email
+-- );
